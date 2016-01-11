f4972eddd4f5cb454e7173b1e815491fd7b875da
Huang Jian-Yu <bubu19861213@gmail.com>
2016-01-12 00:20:56 +0800

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by Huang Jian-Yu on 2016/1/12.
+//  Copyright © 2016年 Zitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
